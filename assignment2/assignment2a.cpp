fbbc3f3249fb137074d7a6bfec66387af55af8ef
Ellie R <esr07.san@outlook.com>
2024-06-26 11:59:28 -0700

    assignment 2

---
+++ assignment2/assignment2a.cpp
@@ -0,0 +1,13 @@
+// Ellie Rozenkrants
+// Assignment 2a
+// June 25
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hi my name is Ellie" << endl;
+
+    return 0;
+}
---
+++ assignment2/asssignment2b.cpp
@@ -0,0 +1,19 @@
+// Ellie Rozenkrants
+// Assignment 2b
+// June 26
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num1 = 5;
+   
+   int num2 = 4;
+   
+   int sum = num1 + num2;
+   
+   cout << num1 << " + " << num2 << " is equal to " << sum << endl;
+
+    return 0;
+}
